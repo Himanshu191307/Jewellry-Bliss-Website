21141f59e8111df11c862ba1286f949d52641134
Himanshu191307 <himanshu19320@gmail.com>
2023-09-27 18:39:19 +0530

    JEWELLERY WEBSITE

---
+++ js/javacode.js
@@ -0,0 +1,24 @@
+
+
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
